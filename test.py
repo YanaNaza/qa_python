7be91ab04a3b6a8a081c0124575c20d14120d819
Yana Naz <proffen@yandex.ru>
2024-04-17 22:18:29 +1000

    Тестовые покрытия класса BooksCollector

---
+++ test.py
@@ -0,0 +1,4 @@
+[develop 2d0c5e5] Добавлен новый код для тестирования
+ 3 files changed, 113 insertions(+), 1 deletion(-)
+ create mode 100644 __pycache__/main.cpython-312.pyc
+ create mode 100644 __pycache__/tests.cpython-312-pytest-8.1.1.pyc
